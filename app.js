6df980506c9c3bcc2665848861945367ef4edf0e
Sankaraleti <sankaraleti143@gmail.com>
2025-02-15 20:39:42 +0530

    added custom middleware for database errors

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middileware.js";
+import cookieParser from "cookie-parser";
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser);
 app.get("/", (req, res) => {
   res.send({ body: "welcome to the  Subscription tracker api" });
 });
@@ -14,6 +19,8 @@ app.get("/", (req, res) => {
 app.use("/api/v1/auth", authRouter);
 app.use("api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
   console.log(`Subscription tracker listening http://localhost:${PORT}`);
