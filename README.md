179b708f9ca2632b3e436725819fb7ff6970ade7
Sankaraleti <sankaraleti143@gmail.com>
2025-02-15 15:49:43 +0530

    added database connection

--- README.md
+++ README.md
@@ -13,3 +13,9 @@
 # adding routes -> create routes folder
 
 # separate routes based on the functionality
+
+# setting up data (login into mangodb atlas)
+
+# Create new project & create a cluster
+
+# create database folder -> mongodb.js file for database connection
--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { PORT } from "./config/env.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 const app = express();
 
 app.get("/", (req, res) => {
@@ -14,8 +15,9 @@ app.use("/api/v1/auth", authRouter);
 app.use("api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Subscription tracker listening http://localhost:${PORT}`);
+  await connectToDatabase();
 });
 
 export default app;
